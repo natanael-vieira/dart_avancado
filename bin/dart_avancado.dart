eea8b11c9f2332ea59ecf049d59dea1a1796a595
Natanael Vieira <natanaelsales@live.com>
2022-08-11 18:57:16 -0300

    Initial commit

---
+++ bin/dart_avancado.dart
@@ -0,0 +1,5 @@
+import 'package:dart_avancado/dart_avancado.dart' as dart_avancado;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_avancado.calculate()}!');
+}
---
+++ test/dart_avancado_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_avancado/dart_avancado.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
